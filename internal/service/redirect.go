357977fe1a89e1381a0379dec3fc4bb5fe86c17c
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2023-12-03 15:01:40 -0300

    get rules from file + goroutine to update rules

--- internal/service/redirect.go
+++ internal/service/redirect.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
+	"time"
 	"traefik-redirect-engine/internal/model"
 	"traefik-redirect-engine/internal/utils"
 )
@@ -13,4 +15,47 @@ func GetDestinyUrl(rule model.Rule, req *http.Request) string {
 	result := strings.ReplaceAll(fullUrl, req.Host, rule.Destiny)
 
 	return result
+}
+
+func GetRules(config *model.Config) map[string]model.Rule {
+	switch config.Source {
+	case "file":
+		return GetRulesFromFile(config.FilePath)
+	}
+
+	return make(map[string]model.Rule)
+}
+
+func GetRulesFromFile(filePath string) map[string]model.Rule {
+	result := make(map[string]model.Rule)
+
+	body, err := utils.Readfile(filePath)
+	if err != nil {
+		return result
+	}
+
+	err = json.Unmarshal(body, &result)
+
+	if err != nil {
+		return result
+	}
+
+	return result
+}
+
+func StartUpdateRedirectRulesJob(config *model.Config, redirectRules map[string]model.Rule) {
+	go func() {
+
+		for {
+			updateRedirectRules(redirectRules, GetRules(config))
+			time.Sleep(30 * time.Second)
+		}
+	}()
+
+}
+
+func updateRedirectRules(redirectRules map[string]model.Rule, newRules map[string]model.Rule) {
+	for key, value := range newRules {
+		redirectRules[key] = value
+	}
 }
