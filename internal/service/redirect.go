22d9687bf4245b6ea3fff9ad0cae1f18e36aad42
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2023-12-08 13:48:25 -0300

    add logs

--- internal/service/redirect.go
+++ internal/service/redirect.go
@@ -3,6 +3,7 @@ package service
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -19,6 +20,9 @@ func GetDestinyUrl(rule model.Rule, req *http.Request) string {
 }
 
 func GetRules(config *model.Config) map[string]model.Rule {
+	if !config.IsRedirectEnable {
+		return make(map[string]model.Rule)
+	}
 	switch config.Source {
 	case "file":
 		return GetRulesFromFile(config.FilePath)
@@ -32,14 +36,19 @@ func GetRulesFromFile(filePath string) map[string]model.Rule {
 
 	body, err := utils.Readfile(filePath)
 	if err != nil {
+		os.Stderr.WriteString("Error on read rules file: " + err.Error())
 		return result
 	}
 
 	err = json.Unmarshal(body, &result)
 
 	if err != nil {
+		os.Stderr.WriteString("Error on unmarshal rules file " + err.Error())
+
 		return result
 	}
+
+	os.Stdout.WriteString("Success loaded rules")
 
 	return result
 }
